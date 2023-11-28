954816d04656c5b28d0ff781311319cecea3bbb8
dollyishere <109561384+dollyishere@users.noreply.github.com>
2023-11-28 23:50:27 +0900

    [level unrated] Title: 문자열의 앞의 n글자, Time: 15.11 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, n: Int): String {
+        var answer: String = ""
+        
+        answer = my_string.slice(0 until n)
+        
+        return answer
+    }
+}
