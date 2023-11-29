19f8e5af27767777cdcc77b58b4586a3ec574fca
dollyishere <109561384+dollyishere@users.noreply.github.com>
2023-11-30 01:52:34 +0900

    [level unrated] Title: 배열 만들기 1, Time: 9.65 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(n: Int, k: Int): IntArray {
         var answer: IntArray = intArrayOf()
-        
+        // step으로 일정 간격 두고 값 올리기
         for (i in k .. n step k) {
             answer += i
         }
