64ff0b41df586df2964aaad9e269e64e3cda2ca0
Juyeon Lim <109561384+dollyishere@users.noreply.github.com>
2023-12-06 01:15:31 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 22.34 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(myString: String, pat: String): Int {
+        var answer: Int = 0
+        var temp: String = ""
+        
+        for (i in 0 until myString.length) {
+            temp += myString[i]
+            if (temp.length == pat.length) {
+                if (temp == pat) {
+                    answer ++
+                }
+                temp = temp.slice(1 until temp.length)
+            }
+        }
+        return answer
+    }
+}
