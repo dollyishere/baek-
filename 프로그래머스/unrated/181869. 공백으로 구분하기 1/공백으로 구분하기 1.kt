b111c977b85872e6082a3417acc8ae3eed905398
Juyeon Lim <109561384+dollyishere@users.noreply.github.com>
2023-12-06 01:25:51 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 28.23 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        var temp:String = ""
+        
+        for (i in 0 until my_string.length) {
+            if (my_string[i] == ' ') {
+                answer += temp
+                temp = ""
+            } else {
+                temp += my_string[i]
+            }
+        }
+        
+        if (temp != "") {
+            answer += temp
+        }
+        
+        return answer
+    }
+}
