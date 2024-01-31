fdde2f34c964079deebd85dc0d71c424277b8388
Juyeon Lim <109561384+dollyishere@users.noreply.github.com>
2024-01-31 23:53:03 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(dot: IntArray): Int {
+        var answer: Int = when {
+            dot[0] > 0 && dot[1] > 0 -> 1
+            dot[0] < 0 && dot[1] > 0 -> 2
+            dot[0] < 0 && dot[1] < 0 -> 3
+            else -> 4
+        }
+        return answer
+    }
+}
