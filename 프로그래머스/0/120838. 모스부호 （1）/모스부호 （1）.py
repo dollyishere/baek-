0a4683a1d70262c71e4ab6eaa75a0fca42b13326
Juyeon Lim <109561384+dollyishere@users.noreply.github.com>
2024-12-15 21:53:04 +0900

    [level 0] Title: 모스부호 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,16 @@
+def solution(letter):
+
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    answer = ''
+    changed_letter = list(letter.split())
+    
+    for l in changed_letter:
+        answer += morse[l]
+        
+    return answer
